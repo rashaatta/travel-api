023cdc2ccc3d092bd1b7f04b47c78128e7c54a6c
Rasha Atta <rashaatta83@gmail.com>
2024-08-21 21:33:55 +0300

    setup travel api

---
+++ app/Http/Controllers/Api/V1/TourController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ToursListRequest;
+use App\Http\Resources\TourResource;
+use App\Models\Travel;
+
+class TourController extends Controller
+{
+    public function index(Travel $travel, ToursListRequest $request)
+    {
+        $tours = $travel->tours()
+            ->when($request->dateFrom, function ($query) use ($request) {
+                $query->where('starting_date', '>=', $request->dateFrom);
+            })
+            ->when($request->dateTo, function ($query) use ($request) {
+                $query->where('starting_date', '<=', $request->dateTo);
+            })
+            ->when($request->priceFrom, function ($query) use ($request) {
+                $query->where('price', '>=', $request->priceFrom * 100);
+            })
+            ->when($request->priceTo, function ($query) use ($request) {
+                $query->where('price', '<=', $request->priceTo * 100);
+            })
+            ->when($request->sortBy && $request->sortOrder, function ($query) use ($request) {
+                $query->orderBy($request->sortBy, $request->sortOrder);
+            })
+            ->orderBy('starting_date')
+            ->paginate();
+
+
+        return TourResource::collection($tours);
+    }
+}
