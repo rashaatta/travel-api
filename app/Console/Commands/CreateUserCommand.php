f59a38820b173f52d4985683724b3e9a01d79163
Rasha Atta <rashaatta83@gmail.com>
2024-08-21 22:07:16 +0300

    create now command for creating new user

---
+++ app/Console/Commands/CreateUserCommand.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rules\Password;
+
+class CreateUserCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'users:create';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new user';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $user['name'] = $this->ask('Name of the new user');
+        $user['email'] = $this->ask('Email of the new user');
+        $user['password'] = $this->secret('Password of the new user');
+        $roleName = $this->choice('Role of the new user', ['admin', 'user'], 1);
+        $role = Role::where('name', $roleName)->first();
+        if (!$role) {
+            $this->error('Role not found');
+            return -1;
+        }
+
+        $validator = Validator::make($user, [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => ['required', Password::defaults()],
+//            'role' => 'required|integer|exists:roles,id'
+        ]);
+
+        if ($validator->fails()) {
+            foreach ($validator->errors()->all() as $error) {
+                $this->error($error);
+            }
+
+            return -1;
+        }
+
+        DB::transaction(function () use ($user, $role) {
+            $user['password'] = Hash::make($user['password']);
+            $newUser = User::create($user);
+            $newUser->roles()->attach($role->id);
+        });
+
+        $this->info('User ' . $user['name'] . ' was created successfully!');
+        return 0;
+    }
+}
